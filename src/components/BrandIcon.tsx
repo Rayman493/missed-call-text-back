425fdc5c3e34fdb97315236c31bf96829f9332af
Drago <drago@example.com>
2026-06-25 00:56:42 -0400

    Replace BrandIcon asset with transparent logo

--- src/components/BrandIcon.tsx
+++ src/components/BrandIcon.tsx
@@ -6,7 +6,7 @@ interface BrandIconProps {
 export default function BrandIcon({ size = 32, className = '' }: BrandIconProps) {
   return (
     <img
-      src="/icon-192.png"
+      src="/replyflow-r-transparent.svg"
       alt="ReplyFlow"
       width={size}
       height={size}
